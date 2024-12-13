cf9429e07c9c5fe1667ad7c495a64bb35e776150
YaraMasoed <yaramasoed6@gmail.com>
2024-12-13 19:33:23 +0200

    Volunteer Task DTO update

--- src/RMI/VolunteerTaskDTO.java
+++ src/RMI/VolunteerTaskDTO.java
@@ -8,17 +8,19 @@ public class VolunteerTaskDTO implements Serializable {
     private String type;
     private String date;
     private boolean completionStatus;
-    
+    private String description;
+
     public VolunteerTaskDTO() {
     }
-    
-    public VolunteerTaskDTO(String taskName, String type, String date, boolean completionStatus) {
+
+    public VolunteerTaskDTO(String taskName, String type, String date, boolean completionStatus, String description) {
         this.taskName = taskName;
         this.type = type;
         this.date = date;
         this.completionStatus = completionStatus;
+        this.description = description;
     }
-    
+
     public VolunteerTaskDTO(int ID, String taskName, String type, String date, boolean completionStatus) {
         this.ID = ID;
         this.taskName = taskName;
@@ -26,44 +28,52 @@ public class VolunteerTaskDTO implements Serializable {
         this.date = date;
         this.completionStatus = completionStatus;
     }
-    
+
     public int getID() {
         return ID;
     }
-    
+
     public void setID(int ID) {
         this.ID = ID;
     }
-    
+
     public String getTaskName() {
         return taskName;
     }
-    
+
     public void setTaskName(String taskName) {
         this.taskName = taskName;
     }
-    
+
     public String getType() {
         return type;
     }
-    
+
     public void setType(String type) {
         this.type = type;
     }
-    
+
     public String getDate() {
         return date;
     }
-    
+
     public void setDate(String date) {
         this.date = date;
     }
-    
+
     public boolean isCompletionStatus() {
         return completionStatus;
     }
-    
+
     public void setCompletionStatus(boolean completionStatus) {
         this.completionStatus = completionStatus;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
