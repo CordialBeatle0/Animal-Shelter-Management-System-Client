74bf4115127a9203a455e39c851e96d088de64b5
CordialBeatle0 <pitosameh@hotmail.com>
2024-12-15 16:35:04 +0200

    final

--- src/RMI/SellingItemRMI.java
+++ src/RMI/SellingItemRMI.java
@@ -9,7 +9,7 @@ public interface SellingItemRMI extends Remote {
     
     void removeSellingItem(SellingItemDTO sellingItemDTO) throws RemoteException;
     
-    void buyItem(int itemID, int quantityRequired, int userID, String payment) throws RemoteException;
+    boolean buyItem(int itemID, int quantityRequired, int userID, String payment) throws RemoteException;
     
     SellingItemDTO viewSellingItem(int ID) throws RemoteException;
     
